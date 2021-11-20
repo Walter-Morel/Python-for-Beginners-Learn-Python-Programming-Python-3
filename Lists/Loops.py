44b129af0481967fc9b8e01bda016da8eb2978b4
Walter-Morel <walter.morel.noguera@gmail.com>
2021-11-20 21:47:39 +0100

    Loops

---
+++ Lists/Loops.py
@@ -0,0 +1,12 @@
+name = input('Indica tu nombre: ')
+apellido = input('Indica tu apellido: ')
+cargo = input('indica tu profesión: ')
+
+list_date = [f'{name}', f'{apellido}', f'{cargo}']
+for list_upper in list_date:
+    #print(list_upper)
+    #l_upper = int(len(list_upper))
+    print(list_upper[0])
+    #print('El texto {}, tiene {} caracteres.'.format(list_upper, l_upper))
+    
+
