26274bdadb4f8990f749a1941a48c97d98d356b6
Walter-Morel <walter.morel.noguera@gmail.com>
2021-11-20 22:01:41 +0100

    Sorting and ranges

---
+++ Lists/SortingAndRanges.py
@@ -0,0 +1,7 @@
+name = ['Walker', 'Anne', 'Zoraya', 'Ylsa', 'Marry', 'Betty', 'Misael', 'Volkart', 'Mark']
+#name.sort()
+for i in range(0, len(name), 2):
+    print(name[i])
+    #print('Luego de utilizar el metodo sort: {}'.format(i))
+
+    
