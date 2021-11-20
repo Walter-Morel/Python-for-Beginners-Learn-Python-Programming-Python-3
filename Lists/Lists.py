5ef4a827ccbb8e5156e9b5bd3b880d1201acaa49
Walter-Morel <walter.morel.noguera@gmail.com>
2021-11-20 19:47:59 +0100

    Probando las listas

---
+++ Lists/Lists.py
@@ -0,0 +1,12 @@
+linux_distro = ['ubuntu', 'debian', 'arch', 'manjaro']
+linux_distro.extend(['red hat', 'Lite'])
+print(linux_distro)
+'''print(linux_distro[1])
+print(linux_distro[2])
+print(linux_distro[3])'''
+
+
+mas_distro = ['fedora', 'suse', 'centOS']
+linux_distro.extend(mas_distro)
+
+print(linux_distro)
