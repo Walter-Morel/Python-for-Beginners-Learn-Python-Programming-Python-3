ae4b15204af897f5cfaa10141aa2a6115b47ec1b
Walter-Morel <walter.morel.noguera@gmail.com>
2021-11-21 15:04:39 +0100

    Probando las tuplas y listas

---
+++ Tuples/Tuples.py
@@ -0,0 +1,16 @@
+'''dias_de_la_semana = ('Lunes', 'Martes', 'Miercoles',
+'Jueves', 'Viernes', 'Sabado', 'Domingo')
+
+martes = dias_de_la_semana[1]
+print(martes)
+
+
+for dias in dias_de_la_semana:
+    print(dias)
+    print()'''
+
+semanas_tuplas = ('Sabado', 'Domingo')
+semanas_listas = list(semanas_tuplas)
+
+print('semanas_tuplas es {}.'.format(type(semanas_tuplas)))
+print('semanas_listas es {}.'.format(type(semanas_listas)))
