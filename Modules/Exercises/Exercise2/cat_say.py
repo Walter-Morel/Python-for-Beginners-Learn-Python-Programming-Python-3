bef1d204350d11e6071affc67033d3359890b428
Walter Morel <walter.morel.noguera@gmail.com>
2021-11-22 12:52:40 +0100

    Creando modulo para poder llamarlo desde cat_talk.py

---
+++ Modules/Exercises/Exercise2/cat_say.py
@@ -0,0 +1,11 @@
+def cat_say(text):
+#creamos una variable que se encargue de contar el catacter total de la variable $text
+    text_length = len(text)
+
+    print('         {}'.format('_' * text_length))
+    print('       < {} >'.format(text))
+    print('         {}'.format('-' * text_length))
+    print('        /')
+    print(' /\_/\ /')
+    print('( o.o )')
+    print(' > ^ <')
---
+++ Modules/Exercises/Exercise2/cat_talk.py
@@ -0,0 +1,7 @@
+import cat_say
+
+def main():
+    cat_say.cat_say(input('Dime algo: '))
+
+if __name__ == '__main__':
+    main()
